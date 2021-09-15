5e933f6ebb78ce43b7599303ea4fd95831959f13
Jaime Albornoz <albornozsotojaime@gmail.com>
2021-09-15 15:02:07 -0300

    Adding a storage stack

--- src/lambda.js
+++
@@ -1,7 +0,0 @@
-export async function handler(event) {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: `Hola Jaime Albornoz!Your request was received at ${event.requestContext.time}.`,
-  };
-}
