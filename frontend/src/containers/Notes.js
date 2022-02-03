768c129318ea119878f7fea1447ea9a727b015ca
Jaime Albornoz <albornozsotojaime@gmail.com>
2022-02-03 16:33:22 -0300

    delete a note

--- frontend/src/containers/Notes.js
+++ frontend/src/containers/Notes.js
@@ -108,19 +108,44 @@ export default function Notes() {
   //   setIsLoading(true);
   // }
 
+  function deleteNote() {
+    return API.del("notes", `/notes/${id}`);
+  }
+  
   async function handleDelete(event) {
     event.preventDefault();
-
+  
     const confirmed = window.confirm(
       "Are you sure you want to delete this note?"
     );
-
+  
     if (!confirmed) {
       return;
     }
-
+  
     setIsDeleting(true);
+  
+    try {
+      await deleteNote();
+      history.push("/");
+    } catch (e) {
+      onError(e);
+      setIsDeleting(false);
+    }
   }
+  // async function handleDelete(event) {
+  //   event.preventDefault();
+
+  //   const confirmed = window.confirm(
+  //     "Are you sure you want to delete this note?"
+  //   );
+
+  //   if (!confirmed) {
+  //     return;
+  //   }
+
+  //   setIsDeleting(true);
+  // }
 
   return (
     <div className="Notes">
