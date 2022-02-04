c7af62f7f2f2cdd5cc5bb759f5f611e01682cf9e
Jaime Albornoz <albornozsotojaime@gmail.com>
2022-02-04 09:48:43 -0300

    redirect on login

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -7,6 +7,8 @@ import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
 import Notes from "./containers/Notes";
 import Settings from "./containers/Settings";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default function Routes() {
   return (
@@ -14,7 +16,22 @@ export default function Routes() {
       <Route exact path="/">
         <Home />
       </Route>
-      <Route exact path="/login">
+      <UnauthenticatedRoute exact path="/login">
+        <Login />
+      </UnauthenticatedRoute>
+      <UnauthenticatedRoute exact path="/signup">
+        <Signup />
+      </UnauthenticatedRoute>
+      <AuthenticatedRoute exact path="/settings">
+        <Settings />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/notes/new">
+        <NewNote />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/notes/:id">
+        <Notes />
+      </AuthenticatedRoute>
+      {/* <Route exact path="/login">
         <Login />
       </Route>
       <Route exact path="/signup">
@@ -28,7 +45,7 @@ export default function Routes() {
       </Route>
       <Route exact path="/notes/:id">
         <Notes />
-      </Route>
+      </Route> */}
       Finally, catch all unmatched routes
       <Route>
         <NotFound />
---
+++ frontend/src/components/UnauthenticatedRoute.js
@@ -0,0 +1,46 @@
+import React, { cloneElement } from "react";
+import { Route, Redirect } from "react-router-dom";
+import { useAppContext } from "../lib/contextLib";
+
+function querystring(name, url = window.location.href) {
+  const parsedName = name.replace(/[[]]/g, "\\$&");
+  const regex = new RegExp(`[?&]${parsedName}(=([^&#]*)|&|#|$)`, "i");
+  const results = regex.exec(url);
+
+  if (!results || !results[2]) {
+    return false;
+  }
+
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+export default function UnauthenticatedRoute(props) {
+  const { children, ...rest } = props;
+  const { isAuthenticated } = useAppContext();
+  const redirect = querystring("redirect");
+
+  return (
+    <Route {...rest}>
+      {!isAuthenticated ? (
+        cloneElement(children, props)
+      ) : (
+        <Redirect to={redirect ? redirect : "/"} />
+      )}
+    </Route>
+  );
+}
+// export default function UnauthenticatedRoute(props) {
+//   const { children, ...rest } = props;
+//   const { isAuthenticated } = useAppContext();
+
+
+//   return (
+//     <Route {...rest}>
+//       {!isAuthenticated ? (
+//         cloneElement(children, props)
+//       ) : (
+//         <Redirect to="/" />
+//       )}
+//     </Route>
+//   );
+// }
